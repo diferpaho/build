e6cfffb6e247cd574cbc9669128f406518692ca7
diferpaho <diferpaho@gmail.com>
2020-06-26 10:32:53 -0500

    primera parte

---
+++ precache-manifest.5ff40482f16f181c7f61a9fabb588d15.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc7d88e1601b48a4bfc4909532902a06",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c24134b07d28536bef9a",
+    "url": "/static/css/main.40b804a2.chunk.css"
+  },
+  {
+    "revision": "aca4728cd085848b2f5e",
+    "url": "/static/js/2.ae22192e.chunk.js"
+  },
+  {
+    "revision": "a1a636632721a402612df3e043dd630d",
+    "url": "/static/js/2.ae22192e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c24134b07d28536bef9a",
+    "url": "/static/js/main.c3cfaeca.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "86e54d4aff65193610cbe7e9c661e102",
+    "url": "/static/media/favicon-96x96.86e54d4a.png"
+  },
+  {
+    "revision": "435463c1ac85763d2defc671e17c100f",
+    "url": "/static/media/icon.435463c1.png"
+  }
+]);
